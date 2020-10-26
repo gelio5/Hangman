204892c5da974515281e8a7f35d842bc87288e4f
gelio5 <vlreznik97@gmail.com>
2020-10-27 01:16:44 +0300

    Stage 2
    Stage 2 is done

--- task/hangman/hangman.py
+++ task/hangman/hangman.py
@@ -3,4 +3,13 @@ def start():
     print('The game will be available soon.')
 
 
+def ask(check_answer):
+    if check_answer == input('> '):
+        print('You survived!')
+    else:
+        print('You lost!')
+
+
+correct_answer = 'python'
 start()
+ask(correct_answer)
